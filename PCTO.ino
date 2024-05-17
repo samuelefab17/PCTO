b535c00dc6bfe95905dadf1d7dfd4d35322519da
samuelefab17 <samuelefab17@gmail.com>
2024-05-17 10:00:47 +0200

    Test3.9_PCTO
    Some changes

--- PCTO.ino
+++ PCTO.ino
@@ -9,14 +9,15 @@ int pin4 = 7u; //GP 7
 
 //################## START SERIAL ################
 
-int chose_serial_begin = 1; 
+int chose_serial_begin = 0; 
 /* If the value is equal to 0, 9600 will be used,
 while if it is equal to 1, 115200 will be used*/ 
 
 //################### START LOOP #################
 
 int chose_start_loop = 1;
-/*The value represents up to which loop it won't be triggered (value min 1[every loop start], max 6)*/
+/*The value represents up to which loop it won't be triggered 
+(value min 1[All loop start], max 7[All additional loops are inactive])*/
 
 //#################### LOOP TIME ##################
 
@@ -46,42 +47,47 @@ void setup() {
 
   // ################ SETUP LOOP ###################
   if(chose_start_loop == 1){
-    Scheduler.startLoop(loop2);
-    Scheduler.startLoop(loop3);
-    Scheduler.startLoop(loop4);
-    Scheduler.startLoop(loop5);
-    Scheduler.startLoop(loop6);
+      Scheduler.startLoop(loop2);
+      Scheduler.startLoop(loop3);
+      Scheduler.startLoop(loop4);
+      Scheduler.startLoop(loop5);
+      Scheduler.startLoop(loop6);
   }
   else if(chose_start_loop == 2){
-    Scheduler.startLoop(loop3);
-    Scheduler.startLoop(loop4);
-    Scheduler.startLoop(loop5);
-    Scheduler.startLoop(loop6);
+      Scheduler.startLoop(loop3);
+      Scheduler.startLoop(loop4);
+      Scheduler.startLoop(loop5);
+      Scheduler.startLoop(loop6);
   }
     else if(chose_start_loop == 3){
-    Scheduler.startLoop(loop2);
-    Scheduler.startLoop(loop4);
-    Scheduler.startLoop(loop5);
-    Scheduler.startLoop(loop6);
+      Scheduler.startLoop(loop2);
+      Scheduler.startLoop(loop4);
+      Scheduler.startLoop(loop5);
+      Scheduler.startLoop(loop6);
   }
     else if(chose_start_loop == 4){
-    Scheduler.startLoop(loop2);
-    Scheduler.startLoop(loop3);
-    Scheduler.startLoop(loop5);
-    Scheduler.startLoop(loop6);
+      Scheduler.startLoop(loop2);
+      Scheduler.startLoop(loop3);
+      Scheduler.startLoop(loop5);
+      Scheduler.startLoop(loop6);
   }
     else if(chose_start_loop == 5){
-    Scheduler.startLoop(loop2);
-    Scheduler.startLoop(loop3);
-    Scheduler.startLoop(loop4);
-    Scheduler.startLoop(loop6);
+      Scheduler.startLoop(loop2);
+      Scheduler.startLoop(loop3);
+      Scheduler.startLoop(loop4);
+      Scheduler.startLoop(loop6);
   }
     else if(chose_start_loop == 6){
-    Scheduler.startLoop(loop2);
-    Scheduler.startLoop(loop3);
-    Scheduler.startLoop(loop4);
-    Scheduler.startLoop(loop5);
+      Scheduler.startLoop(loop2);
+      Scheduler.startLoop(loop3);
+      Scheduler.startLoop(loop4);
+      Scheduler.startLoop(loop5);
   }
+    else if(chose_start_loop == 7){
+     //All loops except the main one are inactive
+  }
+
+  
 
 }
 
@@ -89,8 +95,10 @@ void setup() {
 void loop() {
   
  digitalWrite(pin1, HIGH);
+ digitalWrite(LED_BUILTIN, HIGH);
  delay(LOOP_TIME);
  digitalWrite(pin1, LOW);
+ digitalWrite(LED_BUILTIN, LOW);
  delay(LOOP_TIME);
 
 }
@@ -134,10 +142,10 @@ void loop4(){
 // Task n.5
 void loop5(){
 
- float number1_LOOP5 = random(9999) / 1000.0;
- float number2_LOOP5 = random(9999) / 1.0;
- float number3_LOOP5 = random(9999) / 100.0;
- float number4_LOOP5 = random(9999) / 10.0;
+ float number1_LOOP5 = random(99999) / 100000.0;
+ float number2_LOOP5 = random(99999) / 1000.0;
+ float number3_LOOP5 = random(99999) / 10000.0;
+ float number4_LOOP5 = random(99999) / 1000.0;
 
  float result1_LOOP5 = (number1_LOOP5 * number4_LOOP5) / (number2_LOOP5 * number3_LOOP5);
  float result2_LOOP5 = sqrt(result1_LOOP5);
