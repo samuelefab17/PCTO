cff10c43d648e1d26680c82d0935a0064d06f5bb
samuelefab17 <samuelefab17@gmail.com>
2024-06-06 13:18:44 +0200

    Update PCTO.ino

--- PCTO.ino
+++ PCTO.ino
@@ -1,155 +1,164 @@
-
 #include <Scheduler.h>
-#include "Lib.h"
+#include "MPU9250.h"
 
- int pin1 = 4u; //GP 4
- int pin2 = 5u; //GP 5
- int pin3 = 6u; //GP 6 
- int pin4 = 7u; //GP 7
+float acce[12];
+float magn[12];
 
- #define SERIAL_BAUD 9600
- //#define SERIAL_BAUD 115200
+int acce_index;
+int magn_index;
 
- #define LOOP2 1
- #define LOOP3 1
- #define LOOP4 1
- #define LOOP5 1
- #define LOOP6 1
- #define KEEP_ALIVE 1
+float ris_acce;
+float ris_magn;
 
- #define LOOP1_TIME 200 //ms
- #define LOOP2_TIME 200
- #define LOOP3_TIME 200  
- #define LOOP4_TIME 200      
- #define LOOP5_TIME 1000 
- #define LOOP6_TIME 1001   
+MPU9250 mpu;
 
- #define DUTY_CYCLE 50 //%
+//#define SERIAL_BAUD 9600
+#define SERIAL_BAUD 115200
 
-void function_pin(int, int, int);
-void function_pin(int pin, int LOOP_TIME, int CYCLE) {
+#define SERIAL_BAUD_COMMUNICATION 9600 //GP 0
+//#define SERIAL_BAUD_COMMUNICATION 115200
 
-  int LOOP_TIME_DUTY_CYCLE = (LOOP_TIME * CYCLE) / 100;
-  digitalWrite(pin, HIGH);
-  delay(LOOP_TIME_DUTY_CYCLE);
-  digitalWrite(pin, LOW);
-  delay(LOOP_TIME_DUTY_CYCLE);
+#define LOOP2 1
+#define LOOP3 1
+#define LOOP4 1
+#define LOOP5 0
+#define LOOP6 0
+#define KEEP_ALIVE 1
 
-}
+#define LOOP1_TIME 100 //ms
+#define LOOP2_TIME 1000  
+#define LOOP3_TIME 100  
+#define LOOP4_TIME 1000       
+#define LOOP5_TIME 1000 
+#define LOOP6_TIME 2000
 
-void setup() {
-  
-   pinMode(pin1, OUTPUT);
-   pinMode(pin2, OUTPUT);
-   pinMode(pin3, OUTPUT);
-   pinMode(pin4, OUTPUT);
-  
-   Serial.begin(SERIAL_BAUD);
+#define ELEM 10 //divisore
 
-   #if LOOP2 == 1
-     Scheduler.startLoop(loop2);
-   #endif
+void setup() 
+{
+  Serial.begin(SERIAL_BAUD);
+  Serial1.begin(SERIAL_BAUD_COMMUNICATION);
 
-   #if LOOP3 == 1
-     Scheduler.startLoop(loop3);
-   #endif
+  #if LOOP2 == 1
+    Scheduler.startLoop(loop2);
+  #endif
 
-   #if LOOP4 == 1
-     Scheduler.startLoop(loop4);
-   #endif
+  #if LOOP3 == 1
+    Scheduler.startLoop(loop3);
+  #endif
 
-   #if LOOP5 == 1
-     Scheduler.startLoop(loop5);
-   #endif
+  #if LOOP4 == 1
+    Scheduler.startLoop(loop4);
+  #endif
 
-   #if LOOP6 == 1
-     Scheduler.startLoop(loop6);
-   #endif
+  #if LOOP5 == 1
+    Scheduler.startLoop(loop5);
+  #endif
 
-   #if KEEP_ALIVE == 1
-     Scheduler.startLoop(keep_alive);
-   #endif
+  #if LOOP6 == 1
+    Scheduler.startLoop(loop6);
+  #endif
 
+  #if KEEP_ALIVE == 1
+    Scheduler.startLoop(keep_alive);
+  #endif
+
+  Wire.begin();
+  mpu.setup(0x68);
+
+  if (!mpu.setup(0x68)) 
+  {  
+    while (1) 
+    {
+      Serial.println("MPU connection failed.");
+      delay(5000);
+    }
+  }
+    mpu.verbose(true); 
+    mpu.calibrateAccelGyro();
+    delay(2500);
+    mpu.calibrateMag();
+    delay(500);
+    mpu.verbose(false);
 }
 
 // Task n.1
-void loop() {
-
-   function_pin(pin1, LOOP1_TIME, DUTY_CYCLE);
-
+void loop() 
+{
+  if (mpu.update())
+  {
+    static uint32_t prev_ms = millis();
+    if (millis() > prev_ms + LOOP1_TIME) 
+    {
+      float lett = mpu.getMagX();
+      float let = mpu.getAccZ();
+      acce[acce_index] = let;
+      magn[magn_index] = lett;
+      acce_index++;
+      magn_index++;
+      prev_ms = millis();
+    }    
+  }
 }
-
+        
 // Task n.2
-void loop2() {
-
-   function_pin(pin2, LOOP2_TIME, DUTY_CYCLE);
-
+void loop2() 
+{
+  float sum;
+  if(acce_index == (ELEM - 1))
+  {
+    for (int i = 0; i < ELEM; i++)	
+		 sum = sum + acce[i];
+	  ris_acce = sum /ELEM;
+    sum = 0;
+    acce_index = 0;
+  }
+  delay(10);
 }
 
 // Task n.3
-void loop3() {
-
-   function_pin(pin3, LOOP3_TIME, DUTY_CYCLE);
-
+void loop3() 
+{
+  float sum;
+  if(magn_index == (ELEM - 1))
+  {
+    for (int i = 0; i < ELEM; i++)	
+		  sum = sum + magn[i];
+	  ris_magn = sum /ELEM;
+    sum = 0;
+    magn_index = 0;
+  }
+  delay(10);
 }
 
 // Task n.4
-void loop4(){
-
-   function_pin(pin4, LOOP4_TIME, DUTY_CYCLE);
-
+void loop4()
+{
+  Serial1.print(ris_acce);
+  Serial1.print(" ");
+  Serial1.println(ris_magn);
+  delay(LOOP4_TIME);
 }
 
 // Task n.5
-void loop5(){
-
-   float number1_LOOP5 = random(99999) / 100000.0;
-   float number2_LOOP5 = random(99999) / 1000.0;
-   float number3_LOOP5 = random(99999) / 10000.0;
-   float number4_LOOP5 = random(99999) / 1000.0;
-
-   double result1_LOOP5 = (number1_LOOP5 * number4_LOOP5) / (number2_LOOP5 * number3_LOOP5);
-   double result2_LOOP5 = sqrt(result1_LOOP5);
-   Serial.println(result1_LOOP5);
-
-   delay(LOOP5_TIME);
-
-   yield();
+void loop5()
+{
 
 }
 
 // Task n.6
-void loop6(){
- 
-   int number1_LOOP6;
-   int number2_LOOP6;
-   int number3_LOOP6;
-   int number4_LOOP6;
-
-   number1_LOOP6 = random(9999);
-   number2_LOOP6 = random(9999);
-   number3_LOOP6 = random(9999);
-   number4_LOOP6 = random(9999);
-    
-   double result1_LOOP6 = (number1_LOOP6 * number3_LOOP6) / number2_LOOP6;
-   double result2_LOOP6 = sqrt(result1_LOOP6) + number4_LOOP6;
-
-   Serial.println(result2_LOOP6);
-
-   delay(LOOP6_TIME);
-
-   yield();
+void loop6()
+{
 
 }
 
 // Task n.7
-void keep_alive(){
+void keep_alive()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(10000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
 
-   digitalWrite(LED_BUILTIN, HIGH);
-   delay(10000);
-   digitalWrite(LED_BUILTIN, LOW);
-   delay(100);
-   
-   yield();
-
+  yield();
 }
